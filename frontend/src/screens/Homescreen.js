a52c6f929feac5f295a9a22ff4e34d01737d423f
Sangmesh0011 <78312170+Sangmesh0011@users.noreply.github.com>
2023-12-12 23:02:19 +0530

    listed products

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import products from "../products";
+import Product from "../components/Product";
+import { Row, Col } from "react-bootstrap";
 
 const Homescreen = () => {
   return (
-    <div>
-      Homescreen
-    </div>
-  )
-}
+    <>
+      <h1>Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product.id} sm={12} md={6} lg={3} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default Homescreen
+export default Homescreen;
