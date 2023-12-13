c2d36adc0569ebb136e4c4da124c624c11fb62d9
Sangmesh0011 <78312170+Sangmesh0011@users.noreply.github.com>
2023-12-13 18:22:03 +0530

    Basic mongo atlas setup

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,9 +1,20 @@
 import React from "react";
-import products from "../products";
+import { useState,useEffect } from "react";
+//import products from "../products";
 import Product from "../components/Product";
 import { Row, Col } from "react-bootstrap";
+import axios from 'axios'
 
 const Homescreen = () => {
+  const [products,setProducts]=useState([]);
+
+  useEffect(() => {
+    const fetchProducts=async()=>{
+      const {data}=await axios.get('/api/products')
+      setProducts(data)
+    }
+    fetchProducts()
+}, []);
   return (
     <>
       <h1>Products</h1>
